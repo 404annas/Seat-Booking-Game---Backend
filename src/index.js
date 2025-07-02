4c43556c60a03e1c1a9fb9b1448d8e2cc0aa9d0b
Muhammad Umar <umarabdulhaque@gmail.com>
2025-07-03 00:29:00 +0500

    Feat:add cors

--- src/index.js
+++ src/index.js
@@ -2,23 +2,31 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const fileUpload = require('express-fileupload');
+const fileUpload = require("express-fileupload");
 const app = express();
 
 const origin = process.env.ORIGIN || "http://localhost:5173";
 
-//middle wares 
-app.use(fileUpload({
-  useTempFiles: true,
-  tempFileDir: '/tmp/',
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
-}));
-app.use(cors({
-  origin: origin,
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+//middle wares
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  })
+);
+app.use(
+  cors({
+    origin: [
+      origin,
+      "http://localhost:5174",
+      "https://ccg-game-app.netlify.app",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 //mongoose setup
--- src/routes/game.route.js
+++ src/routes/game.route.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const express = require("express");
 const GameRoute = express.Router();
 
-const GameController = require('../controllers/game.ctrl');
+const GameController = require("../controllers/game.ctrl");
 
-GameRoute.get('/getGameById/:gameId', GameController.GetGame);
-GameRoute.get('/listActiveGames', GameController.ListActiveGames);
-GameRoute.get('/listNonActiveGames', GameController.ListNonActiveGames);
-GameRoute.get('/leaderboard/:gameId' , GameController.GetLeaderboard);
+GameRoute.get("/getGameById/:gameId", GameController.GetGame);
+GameRoute.get("/listActiveGames", GameController.ListActiveGames);
+GameRoute.get("/listNonActiveGames", GameController.ListNonActiveGames);
+GameRoute.get("/listLatestGame", GameController.ListLatestNonActiveGame);
+GameRoute.get("/leaderboard/:gameId", GameController.GetLeaderboard);
 
 module.exports = GameRoute;
